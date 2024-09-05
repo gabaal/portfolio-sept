9f5e4561c26fa01e07548d5c38e0a77fdee9a801
gerald <geraldbaalham@live.co.uk>
2024-09-05 17:39:14 +0100

    complete needs content

--- pages/about/index.js
+++ pages/about/index.js
@@ -112,7 +112,7 @@ const About = () => {
         initial="hidden"
         animate="show"
         exit="hidden"
-        className="hidden xl:flex absolute bottom-0 -left-[370px]"
+        className="hidden xl:flex absolute -bottom-[50px] -left-[80px]"
       >
         <Avatar />
       </motion.div>
