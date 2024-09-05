7618a640730b0fb4c11d1d1c3984ee1c10f54fa9
gerald <geraldbaalham@live.co.uk>
2024-09-05 16:50:22 +0100

    About complete

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -1,5 +1,14 @@
+// Next image
+
+import Image from "next/image";
+import { HiEllipsisHorizontal } from "react-icons/hi2";
+
 const TopLeftImg = () => {
-  return <div>Top Left Image</div>;
+  return (
+    <div className="absolute top-0 left-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px] opacity-50">
+      <Image src="/top-left-img.png" width={400} height={400} alt="" />
+    </div>
+  );
 };
 
 export default TopLeftImg;
